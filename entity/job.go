10beed7e3ce812394a957a2b1c6a344796eebdfd
Junichi Kakisako <kilo.sw@gmail.com>
2022-08-24 00:17:25 +0900

    added entity

---
+++ entity/job.go
@@ -0,0 +1,41 @@
+package entity
+
+import "time"
+
+type JobID int64
+
+type Job struct {
+	ID        JobID     `json:"id" db:"id"`
+	Name      string    `json:"name" db:"name"`
+	Slug      string    `json:"slug" db:"slug"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+type JobSessionID int64
+type JobSessionStatus string
+
+const (
+	JobSessionStatusRunning  JobSessionStatus = "running"
+	JobSessionStatusFinished JobSessionStatus = "finished"
+	JobSessionStatusFailed   JobSessionStatus = "failed"
+)
+
+type JobSession struct {
+	ID         JobSessionID     `json:"id" db:"id"`
+	JobID      JobID            `json:"job_id" db:"job_id"`
+	LastStatus JobSessionStatus `json:"last_status" db:"last_status"`
+	StartAt    time.Time        `json:"start_at" db:"start_at"`
+	EndAt      time.Time        `json:"end_at" db:"end_at"`
+	CreatedAt  time.Time        `json:"created_at" db:"created_at"`
+	UpdatedAt  time.Time        `json:"updated_at" db:"updated_at"`
+}
+
+type JobSessionEventID int64
+
+type JobSessionEvent struct {
+	ID           JobSessionEventID `json:"id" db:"id"`
+	JobSessionID JobSessionID      `json:"job_session_id" db:"job_session_id"`
+	Timestamp    time.Time         `json:"timestamp" db:"timestamp"`
+	Status       JobSessionStatus  `json:"status" db:"status"`
+}
---
+++ entity/user.go
@@ -0,0 +1,13 @@
+package entity
+
+import "time"
+
+type UserID int64
+
+type User struct {
+	ID        UserID    `json:"id" db:"id"`
+	Email     string    `json:"email" db:"name"`
+	Password  string    `json:"password" db:"password"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
